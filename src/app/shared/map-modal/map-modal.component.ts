541949b6cde215318fbc6aec2d36170ff586f043
herodang <bestervn@gmail.com>
2022-06-15 19:19:20 +0700

    update shared

---
+++ src/app/shared/map-modal/map-modal.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-map-modal',
+  templateUrl: './map-modal.component.html',
+  styleUrls: ['./map-modal.component.scss'],
+})
+export class MapModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+  onCancel(){
+
+  }
+}
